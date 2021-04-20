a381eefef97431c3de15e2b609c484dcbe6e3ed7
Daniel Huffman <drhuffman12@gmail.com>
2021-04-20 01:22:35 -0400

    drhuffman12_split_code_and_add_tests Remove auto-generated place-holder test.

--- spec/crystal_ray_tracer_spec.cr
+++ spec/crystal_ray_tracer_spec.cr
@@ -2,10 +2,6 @@ require "./spec_helper"
 
 describe CrystalRayTracer do
   # TODO: Write tests
-
-  pending "works" do
-    false.should eq(true)
-  end
 end
 
 # # TODO: Spectator seems to not work with Windows; I get:
