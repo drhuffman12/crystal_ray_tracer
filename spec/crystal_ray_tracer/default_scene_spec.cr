d55c641c708816d5ccd3a89f412eba6f928dbdbf
Daniel Huffman <drhuffman12@gmail.com>
2021-04-21 22:13:05 -0400

    drhuffman12_more_split_code_and_add_tests convert the few tests to Spectator and remove place-holder test; code cleanup

--- spec/crystal_ray_tracer/default_scene_spec.cr
+++ spec/crystal_ray_tracer/default_scene_spec.cr
@@ -1,10 +1,10 @@
-require "./../spec_helper"
+require "./../spectator_helper"
 
-describe CrystalRayTracer::DefaultScene do
+Spectator.describe CrystalRayTracer::DefaultScene do
   context "#initialize" do
     context "with no params passed in" do
       it "does not raise" do
-        (CrystalRayTracer::DefaultScene.new).should_not be_nil # should_not raise_error
+        expect(CrystalRayTracer::DefaultScene.new).not_to raise_error
       end
     end
   end
